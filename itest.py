4def5ab664ea9f8febeeab3f092e20a3e3c828e4
Thomas Kjelsrud <tkjelsrud@Thomass-MacBook-Air-2.local>
2019-05-03 20:55:45 +0200

    0.1

--- itest.py
+++ itest.py
@@ -1,5 +1,6 @@
 import unittest
 from urllib import request, parse
+import json
 #data = parse.urlencode(<your data dict>).encode()
 
 # Integration test cases
@@ -7,18 +8,35 @@ from urllib import request, parse
 baseURL = "http://notoms.pythonanywhere.com/retro"
 
 class TestRetroIntegration(unittest.TestCase):
+    testId = 0
+
     def testCreateObject(self):
-        data = parse.urlencode({'type':'board', 'json': '{}'}).encode()
-        req = request.Request(baseURL + "/node/0", data=data) # this will make the method "POST"
-        response = request.urlopen(req)
-        res = response.read()
-        print(res)
+        try:
+            data = parse.urlencode({'type':'board', 'json': '{}'}).encode()
+            req = request.Request(baseURL + "/node/0", data=data) # this will make the method "POST"
+            response = request.urlopen(req)
+            res = response.read()
+
+            js = json.loads(res)
+
+            self.assertTrue(js.result == "200")
+            self.assertTrue(js.id > 0)
+
+            self.testId = js.id
+
+        except Exception as error:
+                assert False, "Integration test failed with exception " + str(error)
+
 
     def testLoadObject(self):
-        #
-        with request.urlopen(baseURL + "/node/12") as response:
-            res = response.read()
-            print(res)
+        try:
+            with request.urlopen(baseURL + "/node/" + str(self.testId)) as response:
+                res = response.read()
+                js = json.loads(res)
+        except Exception as error:
+            assert False, "Integration test failed with exception " + str(error)
+
+
 
     def testLoadObjectList(self):
         #
