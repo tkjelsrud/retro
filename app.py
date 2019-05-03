ecb4e37f24e84a13719d5d03936b519fa702373a
Kjelsrud <thomas.kjelsrud@dnb.no>
2019-05-03 12:58:23 +0200

    0.1

--- app.py
+++ app.py
@@ -11,32 +11,30 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DbSetup.getUri()
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-@app.route("/retro/board/<int:board_id>", methods=["GET", "POST", "DELETE"])
-def board(board_id):
+@app.route("/retro/board/", methods=["POST"])
+def board():
     if request.method == "POST":
-        # Create a board
-        # Json properties must be included
-
         b = DbObject(type="board", json="{'title':'Test Board'}")
         db.session.add(b)
         db.session.commit()
 
         return make_response(b.json, 200)
 
+@app.route("/retro/board/<int:board_id>", methods=["GET", "DELETE"])
+def board(board_id):
     if request.method == "GET":
         # Load object which is the board
         # TODO: create a more private way to separate boards (need key to get it)
         b = DbObject.query.filter_by(id=board_id, type="board").one()
 
         if b:
-            print("Found board " + b)
-
+            return make_response(b.json, 200)
             # return
         else:
-            None
+            make_response(jsonify({'result': 'Board not found: ' + str(board_id)}), 404)
             # return
 
-        return make_response(jsonify({'result': 'Some text here' + str(board_id)}), 200)
+        return make_response(jsonify({'result': 'Error getting board: ' + str(board_id)}), 500)
 
 @app.route("/retro", methods=["GET"])
 @app.route("/retro/", methods=["GET"])
@@ -46,3 +44,8 @@ def index():
         #return make_response(jsonify({'result': 'Some text here' + str(app.config["TEST123"])}), 200)
 
         return render_template("index.html")
+
+@app.route("/", methods=["GET"])
+def root():
+    if request.method == "GET":
+        return make_response(jsonify({'result': 'Hello world'}), 200)
