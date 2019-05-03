be78e660bff7423fc218300339e2582fd46263b5
Kjelsrud <thomas.kjelsrud@dnb.no>
2019-05-03 11:46:10 +0200

    0.1

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
 from flask_app import app
 from flask import Flask, render_template, request, url_for, redirect, make_response, jsonify
 import json, datetime
-#from flask_sqlalchemy import SQLAlchemy
+import DbSetup
+import DbObject
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config["DEBUG"] = True
-#app.config["TEST123"] = 0
-
-#db = SQLAlchemy(app)
-
-class JsonModel(object):
-    def as_dict(self):
-       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-class Board(JsonModel):
-    id = 123
-
-class Postit(JsonModel):
-    id = 123
-    #__tablename__ = "notes"
-    #id = db.Column(db.Integer, primary_key=True)
-    #scope = db.Column(db.Integer)
-    #title = db.Column(db.String(1024))
-    #typ = db.Column(db.String(128))
-    #json = db.Column(db.String(8192))
-    #dt = db.Column(db.DateTime, default = datetime.datetime.utcnow())
+app.config["SQLALCHEMY_DATABASE_URI"] = DbSetup.getUri()
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @app.route("/retro/board/<int:board_id>", methods=["GET", "POST", "DELETE"])
 def board(board_id):
+    if request.method == "POST":
+        # Create a board
+        # Json properties must be included
+        
+        b = DbObject(type="board", json="{'title':'Test Board'}")
+        db.session.add(b)
+        db.session.commit()
+
+        return make_response(b.json, 200)
+
     if request.method == "GET":
+        # Load object which is the board
+        # TODO: create a more private way to separate boards (need key to get it)
+        b = DbObject.query.filter(id=board_id, type="board").one()
+
+        if b:
+            print("Found board " + b)
+
+            # return
+        else:
+            None
+            # return
+
         return make_response(jsonify({'result': 'Some text here' + str(board_id)}), 200)
 
 @app.route("/retro", methods=["GET"])
