047cf9d26645c48b8950d38a115df950ffb46028
Kjelsrud <thomas.kjelsrud@dnb.no>
2019-05-08 11:15:50 +0200

    0.1

--- app.py
+++ app.py
@@ -55,7 +55,11 @@ def node(n_id):
 
                 if n:
                     # Update, only support changing the json for now?
-                    n.json = json.dumps(jso)
+                    if isinstance(jso, dict):
+                        n.json = json.dumps(jso)
+                    elif isinstance(jso, str):
+                        n.json = jso
+
                     upd = True
                 else:
                     return make_response(jsonify({'result': 404, 'message': 'Got node to update but not found'}), 200)
